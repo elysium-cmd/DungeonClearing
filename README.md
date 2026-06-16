22e9d0da966c7c3543b92b9b0c960e4b918e62a2
elysium-cmd <eahodge1@proton.me>
2026-06-15 21:02:57 -0700

    Initialized repository

---
+++ README.md
@@ -0,0 +1,3 @@
+# Dungeon Clearing
+
+This is a small dungeon clearing game created using pygame.
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from dungeonclearning!")
+
+
+if __name__ == "__main__":
+    main()
